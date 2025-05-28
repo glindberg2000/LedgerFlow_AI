8c55d589f68ac72776cae5db667bf15857323ff9
Greg Lindberg <greglindberg@gmail.com>
2025-05-28 10:59:29 -0700

    feat(reports): improved category mapping, sorting, and clickable subtotals in all reports; unmapped business categories now highlighted

--- reports/urls.py
+++ reports/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path("irs/", views.irs_report, name="irs_report"),
+    path(
+        "irs/worksheet/<str:worksheet_name>/",
+        views.irs_worksheet_report,
+        name="irs_worksheet_report",
+    ),
     path("business/", views.business_report, name="business_report"),
     path("combined/", views.combined_report, name="combined_report"),
     path("personal/", views.personal_report, name="personal_report"),
+    path("all_categories/", views.all_categories_report, name="all_categories_report"),
 ]
