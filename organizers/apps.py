91bd947551e95deb29ab1f2cda96d507b1b4ece2
Greg Lindberg <greglindberg@gmail.com>
2025-07-13 18:58:14 -0700

    fix(organizer): use pre_delete signal to delete only BinderItems created by OrganizerWorkbook on delete (robust for all delete paths)

--- organizers/apps.py
+++ organizers/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class OrganizersConfig(AppConfig):
     name = "organizers"
     verbose_name = "Organizers"
+
+    def ready(self):
+        import organizers.signals
